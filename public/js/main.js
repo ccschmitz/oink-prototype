3744143b4cc31a236b9bff904c532b9c69d136e9
Mark Garrigan <mark@markgarrigan.com>
2013-03-08 20:38:36 -0600

    Setup asset libraries

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
+// window.Ember = {};
+// Ember.EXTEND_PROTOTYPES = false;
 
+window.App = Ember.Application.create();
+
+App.ApplicationRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    // `controller` is the instance of ApplicationController
+    controller.set('title', "Hello world!");
+  }
+});
+
+App.ApplicationController = Ember.Controller.extend({
+  appName: 'My First Example'
+});
